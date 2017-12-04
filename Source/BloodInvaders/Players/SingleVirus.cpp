6f1a982f7f7ce2a688fdbac5506a91224b3b8dcd
Christian Schnelzer <schnelzer2@gmail.com>
2017-12-04 19:16:45 +0100

    Collision & Health Handling for Virus (still need to adjust health points)

---
+++ Source/BloodInvaders/Players/SingleVirus.cpp
@@ -0,0 +1,81 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SingleVirus.h"
+#include "Engine/CollisionProfile.h"
+#include "Virus.h"
+#include "Classes/Components/StaticMeshComponent.h"
+
+
+// Sets default values
+ASingleVirus::ASingleVirus()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+
+	// Create the mesh component
+	VirusMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("VirusMesh"));
+	VirusMesh->OnComponentHit.AddDynamic(this, &ASingleVirus::OnHit);		// set up a notification for when this component hits something
+	RootComponent = VirusMesh;
+	VirusMesh->SetCollisionProfileName(UCollisionProfile::Pawn_ProfileName);
+}
+
+// Called when the game starts or when spawned
+void ASingleVirus::BeginPlay()
+{
+	Super::BeginPlay();
+	// Enable generating hit events
+	VirusMesh->SetNotifyRigidBodyCollision(true);
+	// Physics
+	//VirusMesh->SetSimulatePhysics(true);
+
+}
+
+// Called every frame
+void ASingleVirus::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	MoveWithFluid();
+
+}
+
+void ASingleVirus::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	if (OtherActor != NULL && OtherActor != this && OtherComp != NULL)
+	{
+		// if we collide with an enemy
+		if (OtherActor->Tags.Contains("Enemy"))
+		{
+			// Get a refernce from the SingleVirus to the VirusPlayer
+			FString parentComponentName = GetParentActor()->GetName();
+			AActor* ParentActor = GetParentActor();
+			if (ParentActor)
+			{
+				AVirus* VirusPlayer = Cast<AVirus>(ParentActor);
+				if (VirusPlayer)
+				{
+					// Damage the player
+					VirusPlayer->DamagePlayer(1);
+				}
+			}
+
+			// If we collide with a neutrophil, destroy it
+			if (OtherActor->Tags.Contains("Neutrophil"))
+			{
+				OtherActor->Destroy();
+			}
+
+			// Destroy the virus (handle other collisions before this!
+			DestroySingleVirus();
+		}
+	}
+}
+
+void ASingleVirus::DestroySingleVirus() {
+	// Remove SingleVirus from viruses array
+	RemoveSingleVirusFromArray();
+
+	//Destroy the SingleVirus
+	Destroy();
+}
+
---
+++ Source/BloodInvaders/Players/SingleVirus.h
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SingleVirus.generated.h"
+
+UCLASS()
+class BLOODINVADERS_API ASingleVirus : public AActor
+{
+	GENERATED_BODY()
+
+	/* The mesh for a SingleVirus*/
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Mesh, meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* VirusMesh;
+	
+public:	
+
+//Methods
+public:	
+	ASingleVirus();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;// Sets default values for this actor's properties
+	
+
+	/* Function to handle collision with blocking objects*/
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
+	UFUNCTION(BluePrintImplementableEvent, Category = Fluid)
+	void MoveWithFluid();
+
+	UFUNCTION(BluePrintImplementableEvent, Category = Virus)
+	void RemoveSingleVirusFromArray();
+
+	/** Returns ProjectileMesh subobject **/
+	FORCEINLINE UStaticMeshComponent* GetVirusMesh() const { return VirusMesh; }
+
+	UFUNCTION(BlueprintCallable, Category = Virus)
+		void DestroySingleVirus();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+};
