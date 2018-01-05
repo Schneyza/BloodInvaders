33a01beb85554db21e7192debce4fd500ae6dd5f
Christian Schnelzer <schnelzer2@gmail.com>
2018-01-05 13:50:17 +0100

    Fix include order in BacteriumProjectile

--- Source/BloodInvaders/BacteriumProjectile.cpp
+++ Source/BloodInvaders/BacteriumProjectile.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "Projectile.h"
 #include "BacteriumProjectile.h"
+#include "Projectile.h"
 #include "Components/StaticMeshComponent.h"
 
 
